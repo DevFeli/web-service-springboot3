0b655da31b4b97a026f55e6eb3c1631b26c5f0ee
Felipe Reis <dev.felipereis@gmail.com>
2024-06-24 12:16:06 -0300

    payment association

--- src/main/java/com/devreis/webservice/config/TestConfig.java
+++ src/main/java/com/devreis/webservice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devreis.webservice.entities.Category;
 import com.devreis.webservice.entities.Order;
 import com.devreis.webservice.entities.OrderItem;
+import com.devreis.webservice.entities.Payment;
 import com.devreis.webservice.entities.Product;
 import com.devreis.webservice.entities.User;
 import com.devreis.webservice.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
